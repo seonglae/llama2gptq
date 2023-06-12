bea3e381080798192ff90ab6c657a585c7a042d8
seonglae <sungle3737@gmail.com>
2023-06-13 00:55:45 +0900

    feat: streamlit chat ui with qa refactoring

--- src/qa.py
+++ src/qa.py
@@ -7,22 +7,23 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.llms import HuggingFacePipeline
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from transformers import AutoTokenizer, pipeline
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 from auto_gptq import AutoGPTQForCausalLM
 
 from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
 
 
-def load_model():
+def load_model(device: str):
   model_id = "TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ"
   model_basename = "Wizard-Vicuna-7B-Uncensored-GPTQ-4bit-128g.no-act-order"
-  tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+  tokenizer = AutoTokenizer.from_pretrained(
+      model_id, use_fast=True, max_length=4096)
   model = AutoGPTQForCausalLM.from_quantized(
       model_id,
       model_basename=model_basename,
       use_safetensors=True,
       trust_remote_code=True,
-      device="cuda:0",
+      device='cuda:0',
       use_triton=False,
       quantize_config=None)
 
@@ -35,14 +36,36 @@ def load_model():
       top_p=0.95,
       repetition_penalty=1,
   )
-  local_llm = HuggingFacePipeline(pipeline=pipe)
-  return local_llm
+  llm = HuggingFacePipeline(pipeline=pipe)
+  return llm
 
 
-def qa(db, embeddings, llm) -> Tuple:
-  query = input("\nQuestion: ")
-  if query == "exit":
-    return ()
+def load_embeddings(device):
+  embeddings = HuggingFaceInstructEmbeddings(
+      model_name="hkunlp/instructor-xl", model_kwargs={"device": device}
+  )
+  return embeddings
+
+
+def load_db(embeddings):
+  db = Chroma(
+      persist_directory=PERSIST_DIRECTORY,
+      embedding_function=embeddings,
+      client_settings=CHROMA_SETTINGS,
+  )
+  print(f"{db._client.list_collections()[0].count()} documents loaded")
+  return db
+
+
+def ask(query, device, db, embeddings, llm) -> Tuple:
+  print(f"Running on: {device}")
+  if embeddings is None:
+    embeddings = load_embeddings(device)
+  if db is None:
+    db = load_db(embeddings)
+  if llm is None:
+    llm = load_model(device)
+
   # input similarity
   start = time()
   input_refs = db.similarity_search(query, search_type="similarity")
@@ -71,22 +94,22 @@ def qa(db, embeddings, llm) -> Tuple:
   print(query)
   print("\n> Answer:")
   print(answer)
-  return (query, input_refs, answer_refs, answer, output_refs)
+  return (input_refs, answer_refs, answer, output_refs)
+
+
+def qa(device, db, embeddings, llm) -> Tuple:
+  query = input("\nQuestion: ")
+  if query == "exit":
+    return ()
+  return (query, *ask(query, device, db, embeddings, llm))
+
 
 def qa_loop(device='cuda'):
-  print(f"Running on: {device}")
-  embeddings = HuggingFaceInstructEmbeddings(
-      model_name="hkunlp/instructor-xl", model_kwargs={"device": device}
-  )
-  db = Chroma(
-      persist_directory=PERSIST_DIRECTORY,
-      embedding_function=embeddings,
-      client_settings=CHROMA_SETTINGS,
-  )
-  print(f"{db._client.list_collections()[0].count()} documents loaded")
-  llm = load_model()
+  embeddings = load_embeddings(device)
+  db = load_db(embeddings)
+  llm = load_model(device)
 
   conversation = []
   while True:
-    conversation.append(qa(db, embeddings, llm))
+    conversation.append(qa(device, db, embeddings, llm))
   return conversation
